11c3ad01b0cb2372b179870192d47af7830e82f2
IamJiyong <din2346@gmail.com>
2023-08-10 17:58:50 +0900

    Fixed bug

--- utils/utils.py
+++ utils/utils.py
@@ -65,3 +65,53 @@ def cvae_multi(pred_traj, target, first_history_index = 0):
         total_loss.append(loss_traj)
     
     return sum(total_loss)/len(total_loss)
+
+
+def convert_bbox(bboxes, bbox_type):
+    # NOTE ltrb to cxcywh
+    if bbox_type == 'cxcywh':
+        bboxes[..., [2, 3]] = bboxes[..., [2, 3]] - bboxes[..., [0, 1]]
+        bboxes[..., [0, 1]] += bboxes[..., [2, 3]]/2
+    # NOTE cxcywh to ltrb
+    elif bbox_type == 'ltrb':
+        bboxes[..., [0, 1]] -= bboxes[..., [2, 3]]/2
+        bboxes[..., [2, 3]] += bboxes[..., [0, 1]] 
+
+    return bboxes    
+
+
+def convert_normalize_bboxes(bboxes, normalize, bbox_type, min_bbox, max_bbox):
+    '''input box type is x1y1x2y2 in original resolution'''
+    assert isinstance(bboxes, np.ndarray) or isinstance(bboxes, torch.Tensor)
+
+    bboxes = convert_bbox(bboxes, bbox_type)
+
+    if normalize == 'zero-one':
+        bboxes = (bboxes - min_bbox) / (max_bbox - min_bbox)
+    elif normalize == 'plus-minus-one':
+        bboxes = (2 * (bboxes - min_bbox) / (max_bbox - min_bbox)) - 1
+    elif normalize == 'none':
+        pass
+    else:
+        raise ValueError(normalize)
+    
+    return bboxes
+
+
+def convert_unnormalize_bboxes(bboxes, normalize, bbox_type, min_bbox, max_bbox):
+    '''input box type is cxcywh in normalized resolution'''
+    assert isinstance(bboxes, np.ndarray) or isinstance(bboxes, torch.Tensor)
+
+    bboxes = convert_bbox(bboxes, bbox_type)
+
+    # NOTE Unnormalize bbox
+    if normalize == 'zero-one':
+        bboxes = (bboxes * (max_bbox - min_bbox)) + min_bbox
+    elif normalize == 'plus-minus-one':
+        bboxes = ((max_bbox - min_bbox) * (bboxes + 1) / 2) + min_bbox
+    elif normalize == 'none':
+        pass
+    else:
+        raise ValueError(normalize)
+
+    return bboxes
