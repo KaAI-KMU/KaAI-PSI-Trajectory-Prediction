ee56f20ffa0a7c3f90ffe64ff908adeb425b4dbb
IamJiyong <din2346@gmail.com>
2023-08-17 14:15:10 +0900

    original sgnet

--- models/traj_modules/model_sgnet_traj_bbox.py
+++ models/traj_modules/model_sgnet_traj_bbox.py
@@ -1,11 +1,9 @@
-import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from utils.utils import rmse_loss, cvae_multi
+from utils.utils import rmse_loss
 from ..model_template import ModelTemplate
-from .model_bitrap_traj_bbox import BiTraPNP
 from ..feature_extractor import *
 
 cuda = True if torch.cuda.is_available() else False
@@ -15,36 +13,21 @@ LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
 class SGNetTrajBbox(ModelTemplate):
-    def __init__(self, model_cfg, dataset):
+    def __init__(self, model_cfg, dataset=None):
         super(SGNetTrajBbox, self).__init__()
-        self.cvae = BiTraPNP(model_cfg.cvae)
-        self.hidden_size = model_cfg.hidden_size # GRU hidden size
-        self.enc_steps = model_cfg.enc_steps # observation step
-        self.dec_steps = model_cfg.dec_steps # prediction step
-        self.dataset = dataset
+
+        self.hidden_size = model_cfg.hidden_size
+        self.enc_steps = model_cfg.enc_steps
+        self.dec_steps = model_cfg.dec_steps
         self.dropout = model_cfg.dropout
-        self.use_speed = model_cfg.get('speed_module', None) is not None
-        self.use_desc = model_cfg.get('description_module', None) is not None
-        
-        self.bbox_module = SgnetFeatureExtractor(model_cfg.bbox_module)
-        self.speed_module = SgnetFeatureExtractor(model_cfg.speed_module) if self.use_speed else None
-        self.desc_module = DescFeatureExtractor(model_cfg.description_module, freeze_params=True) if self.use_desc else None
-        
-        self.pred_dim = model_cfg.pred_dim
-        self.K = model_cfg.K
-        self.map = False
-        if self.dataset in ['PSI2.0','JAAD','PIE']:
-            # the predict shift is in pixel
-            self.pred_dim = 4
-            self.regressor = nn.Sequential(nn.Linear(self.hidden_size, 
-                                                     self.pred_dim),
-                                                     nn.Tanh())
-            self.flow_enc_cell = nn.GRUCell(self.hidden_size*2, self.hidden_size)
-        elif self.dataset in ['ETH', 'HOTEL','UNIV','ZARA1', 'ZARA2']:
-            self.pred_dim = 2
-            # the predict shift is in meter
-            self.regressor = nn.Sequential(nn.Linear(self.hidden_size, 
-                                                        self.pred_dim))   
+        self.feature_extractor = SgnetFeatureExtractor(model_cfg.bbox_module)
+
+        self.pred_dim = 4
+        self.regressor = nn.Sequential(nn.Linear(self.hidden_size, 
+                                                    self.pred_dim),
+                                                    nn.Tanh())
+        self.flow_enc_cell = nn.GRUCell(self.hidden_size*2, self.hidden_size)
+             
         self.enc_goal_attn = nn.Sequential(nn.Linear(self.hidden_size//4,
                                                 1),
                                                 nn.ReLU(inplace=True))
@@ -55,20 +38,18 @@ class SGNetTrajBbox(ModelTemplate):
         self.enc_to_goal_hidden = nn.Sequential(nn.Linear(self.hidden_size,
                                                 self.hidden_size//4),
                                                 nn.ReLU(inplace=True))
-        self.goal_hidden_to_traj = nn.Sequential(nn.Linear(self.hidden_size//4,
-                                                    self.hidden_size),
-                                                    nn.ReLU(inplace=True))
-        self.cvae_to_dec_hidden = nn.Sequential(nn.Linear(self.hidden_size + model_cfg.latent_dim,
-                                                self.hidden_size),
-                                                nn.ReLU(inplace=True))
         self.enc_to_dec_hidden = nn.Sequential(nn.Linear(self.hidden_size,
                                                 self.hidden_size),
                                                 nn.ReLU(inplace=True))
+
 
         self.goal_hidden_to_input = nn.Sequential(nn.Linear(self.hidden_size//4,
                                                     self.hidden_size//4),
                                                     nn.ReLU(inplace=True))
         self.dec_hidden_to_input = nn.Sequential(nn.Linear(self.hidden_size,
+                                                    self.hidden_size),
+                                                    nn.ReLU(inplace=True))
+        self.goal_hidden_to_traj = nn.Sequential(nn.Linear(self.hidden_size//4,
                                                     self.hidden_size),
                                                     nn.ReLU(inplace=True))
         self.goal_to_enc = nn.Sequential(nn.Linear(self.hidden_size//4,
@@ -80,27 +61,25 @@ class SGNetTrajBbox(ModelTemplate):
         self.enc_drop = nn.Dropout(self.dropout)
         self.goal_drop = nn.Dropout(self.dropout)
         self.dec_drop = nn.Dropout(self.dropout)
+
         self.traj_enc_cell = nn.GRUCell(self.hidden_size + self.hidden_size//4, self.hidden_size)
         self.goal_cell = nn.GRUCell(self.hidden_size//4, self.hidden_size//4)
         self.dec_cell = nn.GRUCell(self.hidden_size + self.hidden_size//4, self.hidden_size)
 
         self.criterion = rmse_loss().to(device)
         self.observe_length = model_cfg.observe_length
-        self.forward_ret_dict = {}
-    
+        self.forward_ret_dict = {}        
+
     def SGE(self, goal_hidden):
-        # initial goal input with zero
         goal_input = goal_hidden.new_zeros((goal_hidden.size(0), self.hidden_size//4))
-        # initial trajectory tensor
         goal_traj = goal_hidden.new_zeros(goal_hidden.size(0), self.dec_steps, self.pred_dim)
         goal_list = []
         for dec_step in range(self.dec_steps):
             goal_hidden = self.goal_cell(self.goal_drop(goal_input), goal_hidden)
-            # next step input is generate by hidden
             goal_input = self.goal_hidden_to_input(goal_hidden)
             goal_list.append(goal_hidden)
-            # regress goal traj for loss
             goal_traj_hidden = self.goal_hidden_to_traj(goal_hidden)
+            # regress goal traj for loss
             goal_traj[:,dec_step,:] = self.regressor(goal_traj_hidden)
         # get goal for decoder and encoder
         goal_for_dec = [self.goal_to_dec(goal) for goal in goal_list]
@@ -110,112 +89,64 @@ class SGNetTrajBbox(ModelTemplate):
         goal_for_enc  = torch.bmm(enc_attn, goal_for_enc).squeeze(1)
         return goal_for_dec, goal_for_enc, goal_traj
 
-    def cvae_decoder(self, dec_hidden, goal_for_dec):
-        batch_size = dec_hidden.size(0)
-       
-        K = dec_hidden.shape[1]
-        dec_hidden = dec_hidden.view(-1, dec_hidden.shape[-1])
-        dec_traj = dec_hidden.new_zeros(batch_size, self.dec_steps, K, self.pred_dim)
+    def decoder(self, dec_hidden, goal_for_dec):
+        # initial trajectory tensor
+        dec_traj = dec_hidden.new_zeros(dec_hidden.size(0), self.dec_steps, self.pred_dim)
         for dec_step in range(self.dec_steps):
-            # incremental goal for each time step
-            goal_dec_input = dec_hidden.new_zeros(batch_size, self.dec_steps, self.hidden_size//4)
+            goal_dec_input = dec_hidden.new_zeros(dec_hidden.size(0), self.dec_steps, self.hidden_size//4)
             goal_dec_input_temp = torch.stack(goal_for_dec[dec_step:],dim=1)
             goal_dec_input[:,dec_step:,:] = goal_dec_input_temp
             dec_attn= self.dec_goal_attn(torch.tanh(goal_dec_input)).squeeze(-1)
             dec_attn = F.softmax(dec_attn, dim =1).unsqueeze(1)
-            goal_dec_input  = torch.bmm(dec_attn,goal_dec_input).squeeze(1)
-            goal_dec_input = goal_dec_input.unsqueeze(1).repeat(1, K, 1).view(-1, goal_dec_input.shape[-1])
+            goal_dec_input  = torch.bmm(dec_attn,goal_dec_input).squeeze(1)#.view(goal_hidden.size(0), self.dec_steps, self.hidden_size//4).sum(1)
+            
+            
             dec_dec_input = self.dec_hidden_to_input(dec_hidden)
             dec_input = self.dec_drop(torch.cat((goal_dec_input,dec_dec_input),dim = -1))
             dec_hidden = self.dec_cell(dec_input, dec_hidden)
             # regress dec traj for loss
-            batch_traj = self.regressor(dec_hidden)
-            batch_traj = batch_traj.view(-1, K, batch_traj.shape[-1])
-            dec_traj[:,dec_step,:,:] = batch_traj
+            dec_traj[:,dec_step,:] = self.regressor(dec_hidden)
         return dec_traj
-
-    def encoder(self, raw_inputs, raw_targets, traj_input, flow_input=None, start_index = 0):
+        
+    def encoder(self, traj_input, flow_input=None, start_index = 0):
         # initial output tensor
         all_goal_traj = traj_input.new_zeros(traj_input.size(0), self.enc_steps, self.dec_steps, self.pred_dim)
-        all_cvae_dec_traj = traj_input.new_zeros(traj_input.size(0), self.enc_steps, self.dec_steps, self.K, self.pred_dim)
+        all_dec_traj = traj_input.new_zeros(traj_input.size(0), self.enc_steps, self.dec_steps, self.pred_dim)
         # initial encoder goal with zeros
         goal_for_enc = traj_input.new_zeros((traj_input.size(0), self.hidden_size//4))
         # initial encoder hidden with zeros
         traj_enc_hidden = traj_input.new_zeros((traj_input.size(0), self.hidden_size))
-        total_probabilities = traj_input.new_zeros((traj_input.size(0), self.enc_steps, self.K))
-        total_KLD = 0
         for enc_step in range(start_index, self.enc_steps):
+            
             traj_enc_hidden = self.traj_enc_cell(self.enc_drop(torch.cat((traj_input[:,enc_step,:], goal_for_enc), 1)), traj_enc_hidden)
             enc_hidden = traj_enc_hidden
+            # generate hidden states for goal and decoder 
             goal_hidden = self.enc_to_goal_hidden(enc_hidden)
-            goal_for_dec, goal_for_enc, goal_traj = self.SGE(goal_hidden)
-            all_goal_traj[:,enc_step,:,:] = goal_traj
             dec_hidden = self.enc_to_dec_hidden(enc_hidden)
-            if self.training:
-                cvae_hidden, KLD, probability = self.cvae(dec_hidden, raw_inputs[:,enc_step,:], self.K, raw_targets[:,enc_step,:,:])
-            else:
-                cvae_hidden, KLD, probability = self.cvae(dec_hidden, raw_inputs[:,enc_step,:], self.K)
-            total_probabilities[:,enc_step,:] = probability
-            total_KLD += KLD
-            cvae_dec_hidden= self.cvae_to_dec_hidden(cvae_hidden)
-            if self.map:
-                map_input = flow_input
-                cvae_dec_hidden = (cvae_dec_hidden + map_input.unsqueeze(1))/2
-            all_cvae_dec_traj[:,enc_step,:,:,:] = self.cvae_decoder(cvae_dec_hidden, goal_for_dec)
-        return all_goal_traj, all_cvae_dec_traj, total_KLD, total_probabilities
+
+            goal_for_dec, goal_for_enc, goal_traj = self.SGE(goal_hidden)
+            dec_traj = self.decoder(dec_hidden, goal_for_dec)
+
+            # output 
+            all_goal_traj[:,enc_step,:,:] = goal_traj
+            all_dec_traj[:,enc_step,:,:] = dec_traj
+        
+        return all_goal_traj, all_dec_traj
             
+
     def forward(self, data, training=True):
         bboxes = data['bboxes'][:,:self.observe_length,:].to(device).type(FloatTensor)
-        targets = data['targets'].to(device).type(FloatTensor)
-        self.training = training
-        if torch.is_tensor(0):
-            start_index = start_index[0].item()
-        if self.dataset in ['PSI2.0','JAAD','PIE']:
-            input_list = []
-            bbox_input = self.bbox_module(bboxes)
-            input_list.append(bbox_input)
-            if self.use_speed:
-                speed = data['speed'][:, :self.observe_length, :].to(device).type(FloatTensor)
-                speed_input = self.speed_module(speed)
-                input_list.append(speed_input)
-            if self.use_desc:
-                desc = data['single_description']
-                desc_input = self.desc_module(desc, frame_len=bbox_input.shape[1])
-                input_list.append(desc_input)
-            traj_input = torch.cat(input_list, dim=-1)
-            all_goal_traj, all_cvae_dec_traj, KLD, total_probabilities = self.encoder(bboxes, targets, traj_input)
-        elif self.dataset in ['ETH', 'HOTEL','UNIV','ZARA1', 'ZARA2']:
-            traj_input_temp = self.bbox_module(bboxes[:,start_index:,:])
-            traj_input = traj_input_temp.new_zeros((bboxes.size(0), bboxes.size(1), traj_input_temp.size(-1)))
-            traj_input[:,start_index:,:] = traj_input_temp
-            all_goal_traj, all_cvae_dec_traj, KLD, total_probabilities = self.encoder(bboxes, targets, traj_input, None, start_index)
-        
-        self.forward_ret_dict['all_goal_traj'] = all_goal_traj
-        self.forward_ret_dict['all_cvae_dec_traj'] = all_cvae_dec_traj
-        self.forward_ret_dict['KLD'] = KLD
-        self.forward_ret_dict['total_probabilities'] = total_probabilities
-        self.forward_ret_dict['traj_pred'] = all_goal_traj[:,-1,:,:]
 
+        traj_input = self.feature_extractor(bboxes)
+        all_goal_traj, all_dec_traj = self.encoder(traj_input)
+
+        self.forward_ret_dict['all_goal_traj'] = all_goal_traj
+        self.forward_ret_dict['all_dec_traj'] = all_dec_traj
+        self.forward_ret_dict['traj_pred'] = all_goal_traj[:,-1,:,:]
         return self.forward_ret_dict
     
     def get_loss(self, targets):
-        all_goal_traj = self.forward_ret_dict['all_goal_traj']
-        all_cvae_dec_traj = self.forward_ret_dict['all_cvae_dec_traj']
-        KLD = self.forward_ret_dict['KLD']
-
-        cvae_pred = all_cvae_dec_traj
-        traj_pred = all_goal_traj
-        kld_loss = KLD.mean()
-        cvae_loss = cvae_multi(cvae_pred, targets)
-        goal_loss = self.criterion(traj_pred, targets)
-        traj_loss = kld_loss + cvae_loss + goal_loss
-
-        loss_dict = {
-            'traj_loss': traj_loss,
-            'kld_loss': kld_loss,
-            'cvae_loss': cvae_loss,
-            'goal_loss': goal_loss,
-        }
-
-        return loss_dict
-        
+        goal_loss = self.criterion(self.forward_ret_dict['all_goal_traj'], targets)
+        dec_loss = self.criterion(self.forward_ret_dict['all_dec_traj'], targets)
+        traj_loss = goal_loss + dec_loss
+        return {'traj_loss': traj_loss, 'goal_loss': goal_loss, 'dec_loss': dec_loss}
